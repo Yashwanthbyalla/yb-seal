fb7600fc907fe1f654e5ac05506f667a713b4194
Yashwanth Byalla <yashwanthbyalla95@gmail.com>
2023-04-08 20:21:41 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -29,8 +29,16 @@ function scrollAnimate() {
             }
             // Timing function
             const timingValue = y_item.getAttribute("yb-seal-timing");
+            const timingWidthValue = y_item.getAttribute("yb-seal-timing-width");
             if (timingValue) {
                 y_item.style.transitionTimingFunction = timingValue;
+                y_item.style.transition = 'width 2s' + timingValue;
+                y_item.addEventListener('mouseenter', function () {
+                    y_item.style.width = timingWidthValue + 'px';
+                });
+                y_item.addEventListener('mouseleave', function () {
+                    y_item.style.width = '';
+                });
             }
             if ((Array.from(y_item.classList).includes('yb-seal-slide-up')) || (Array.from(y_item.classList).includes('yb-seal-slide-down'))) {
                 let op = 0.1;
@@ -38,7 +46,7 @@ function scrollAnimate() {
                     op += 0.1;
                     if (op == 1)
                         return;
-                    y_item.style.opacity = op;
+                    y_item.style.opacity = op.toString();
                 }
             }
         }
@@ -50,7 +58,7 @@ function scrollAnimate() {
                     op -= 0.1;
                     if (op == 0)
                         return;
-                    y_item.style.opacity = op;
+                    y_item.style.opacity = op.toString();
                 }
             }
         }
